6113c5928d055c9820302454e27ddaa55589d39c
Michael Peterson <michaeljoepeterson@gmail.com>
2020-10-18 12:53:32 -0600

    updated git ignore

--- functions/routers/main-router.js
+++ functions/routers/main-router.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const functions = require('firebase-functions');
 const {router:formRouter} = require('./form-router');
+const config = functions.config()
 
 router.get('/test',(req,res) => {
     res.status = 200;
-    console.log('hit test router');
+    console.log('hit test router',config);
     return res.json({
         message:'hello world'
     });
