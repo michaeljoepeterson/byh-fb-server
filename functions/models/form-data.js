00a50aa5058147b59b643ee8a644c14595227da8
Michael Peterson <michaeljoepeterson@gmail.com>
2020-11-09 20:52:31 -0700

    initial setup for saving data to db todo time with date make sure it returns correctly for gmt

--- functions/models/form-data.js
+++ functions/models/form-data.js
@@ -1,5 +1,7 @@
 const {BaseData} = require('./base-data');
+//because of nodejs version 10 we have to initialize vars in constructor
 
+//possible todo add dynamic field creation in case fields are added to form
 class FormData extends BaseData{
 
     constructor(data){
@@ -13,9 +15,99 @@ class FormData extends BaseData{
         this.referralTypeAndName = null;
         this.clientLocation = null;
         this.locationNeighbourhood = null;
+        this.country = null;
         this.firstName = null;
         this.age = null;
         this.learnedAbout = null;
+
+    }
+
+    mapData(data){
+        const numberType = 'number';
+        const stringType = 'string';
+        const dateType = 'date';
+        const timeType = 'time';
+        //ids from google form
+        const dataMap = {
+            '449303681':{
+                classKey:'referralNum',
+                type:numberType
+            },
+            '496416008':{
+                classKey:'referralType',
+                type:stringType
+            },
+            '238537915':{
+                classKey:'otherReferralType',
+                type:stringType 
+            },
+            '1762050626':{
+                classKey:'schoolDistrict',
+                type:stringType 
+            },
+            '1313658934':{
+                classKey:'referralDate',
+                type:dateType 
+            },
+            '1532216742':{
+                classKey:'contactTime',
+                type:timeType 
+            },
+            '1677961388':{
+                classKey:'referralPer',
+                type:stringType 
+            },
+            '537383970':{
+                classKey:'referralTypeAndName',
+                type:stringType 
+            },
+            '152041769':{
+                classKey:'clientLocation',
+                type:stringType 
+            },
+            '1553411245':{
+                classKey:'locationNeighbourhood',
+                type:stringType 
+            },
+            '1717973430':{
+                classKey:'country',
+                type:stringType 
+            },
+            '1610778761':{
+                classKey:'firstName',
+                type:stringType 
+            },
+            '57428050':{
+                classKey:'age',
+                type:numberType 
+            },
+            '757950175':{
+                classKey:'learnedAbout',
+                type:stringType 
+            }
+        };
+
+        for(let key in data){
+            if(this.mapData[key]){
+                let {type,classKey} = dataMap[key];
+                if(type === stringType){
+  
+                }
+                else if(type === numberType){
+
+                }
+                else if(type == dateType){
+
+                }
+                else if(type == timeType){
+
+                }
+            }
+        }
+    }
+
+    serialize(){
+        console.log(Object.keys(this));
     }
 }
 
--- functions/tools/checkkey.js
+++ functions/tools/checkkey.js
@@ -1,5 +1,4 @@
 const {SECRET} = require('../config');
-const {FormData} = require('../models/form-data');
 
 const checkKey = (key) => {
     var buf = Buffer.from(key, 'base64').toString();
